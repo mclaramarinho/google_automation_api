459d4881d80499c049ea9e35db5c1a0ce14d79fb
mclaramarinho <marinho.claramb@gmail.com>
2023-12-29 10:12:44 -0300

    fix: added redirect uris and js origins to the credentials + client secret

--- credentials.py
+++ credentials.py
@@ -7,12 +7,13 @@ def get_credentials():
   credentials = {
     "installed": {
       "client_id": os.getenv("CLIENT_ID"),
+      "client_secret": os.getenv("CLIENT_SECRET"),
       "project_id": os.getenv("PROJECT_ID"),
       "auth_uri": "https://accounts.google.com/o/oauth2/auth",
       "token_uri": "https://oauth2.googleapis.com/token",
       "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
-      "client_secret": os.getenv("CLIENT_SECRET"),
-      "redirect_uris": ["http://localhost"]
+      "redirect_uris":["http://127.0.0.1:5000/authCallback","http://127.0.0.1:5000","https://google-automation-api.vercel.app","https://localhost","https://google-automation-api.vercel.app/authCallback", "https://localhost/authCallback"],
+      "javascript_origins":["https://localhost","https://google-automation-api.vercel.app"]
     }
   }
   return credentials
