850faa8e5c10d2a3da6f6f67a6f59f47ce4c32c6
mclaramarinho <marinho.claramb@gmail.com>
2024-01-05 15:13:05 -0300

    fix: redirecting

--- api/main.py
+++ api/main.py
@@ -46,7 +46,7 @@ def auth_callback():
     if req != False:
         res = make_response()
         res.status_code = 302
-        res.set_cookie("daystream_token", value=json.dumps(req), domain=".vercel.app", httponly=False, samesite=None)
+        res.set_cookie("daystream_token", value=json.dumps(req) ,samesite=None)
         res.location = "https://dstream.vercel.app/"
         return res
     else:
