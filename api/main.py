65c8305ee4cb5ad9becfaa276bf53e3e92ed8d00
mclaramarinho <marinho.claramb@gmail.com>
2024-01-05 09:55:52 -0300

    fix: redirecting

--- api/main.py
+++ api/main.py
@@ -47,7 +47,7 @@ def auth_callback():
         res = redirect(location="http://localhost:3000")
         res.status_code = 302
 
-        res.set_cookie("daystream_token", json.dumps(req), httponly=False, secure=False, samesite="Lax", domain="http://localhost:3000/auth")
+        res.set_cookie("daystream_token", json.dumps(req), httponly=False, secure=False, samesite="Lax", domain="http://localhost:3000")
         res.response = {"message": "Success!"}
         return res
     else:
